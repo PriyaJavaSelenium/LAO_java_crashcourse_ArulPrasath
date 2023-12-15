eda6355189548960c77ad6513e8add820e6585fb
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-12-15 13:21:33 +0530

    Create reverse.java
    this program will reverse the string we give

---
+++ reverse.java
@@ -0,0 +1,10 @@
+package learnAutomationOnline;
+
+public class reverse{
+    public static void main(String[]args){
+        String str1 = "JAVA";
+        StringBuffer word = new StringBuffer();
+        word.append(str1);
+        System.out.println(word.reverse());
+    }
+}
